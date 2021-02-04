f4b06a98f4f915e590acc272ef78771bd727028e
Kolan4ick <Kolan4ick@gmail.com>
2021-02-04 15:04:10 +0200

    Add simple optimization.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.with_attached_avatar
   end
 
   def show
     @product = Product.find(params[:id])
-    @products = Product.limit(3)
+    @products = Product.limit(3).with_attached_avatar
   end
 end
