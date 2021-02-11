08b61541d6e2f71bc1db5aca14eb7ecf580cb91d
Kolan4ick <Kolan4ick@gmail.com>
2021-02-12 01:32:48 +0200

    Added style to modal form and fixed dark theme.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import {blackAndWhite} from "./custom"
-import {add_to_cart, show_cart} from "./cart"
+import {add_to_cart, remove_from_cart, show_cart, cart_product_change_count, cart_product_change_price} from "./cart"
 import "../stylesheets/application"
 
 Rails.start()
@@ -19,3 +19,6 @@ ActiveStorage.start()
 window.blackAndWhite = blackAndWhite
 window.add_to_cart = add_to_cart
 window.show_cart = show_cart
+window.remove_from_cart = remove_from_cart
+window.cart_product_change_count = cart_product_change_count
+window.cart_product_change_price = cart_product_change_price
