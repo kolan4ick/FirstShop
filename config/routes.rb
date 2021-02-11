d1fcffeffd70488605dbb304987a43bd40963cb5
Kolan4ick <Kolan4ick@gmail.com>
2021-02-12 01:31:10 +0200

    Added 3 models: order, order_product, user. And wrote the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   resources :products, only: %i[show index] do
     member do
-      post :add_to_cart
+      post :cart, to: 'products#add_to_cart'
+      delete :cart, to: 'products#remove_from_cart'
+      patch :cart, to: 'products#change_count_in_cart'
     end
 
     collection do
       get :cart
     end
   end
+  resources :orders, only: %i[new create]
 end
