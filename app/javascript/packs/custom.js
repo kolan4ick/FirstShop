94728cbac01aa7192889596597e814408573c799
Kolan4ick <Kolan4ick@gmail.com>
2021-02-11 22:42:40 +0200

    Added assets for dark theme.

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,10 +1,13 @@
 export function blackAndWhite() {
     let element = document.getElementById("myid").checked;
     let array = document.getElementById("body").classList;
+    let array2 = document.getElementById("btn-close").classList;
     if (!element) {
         array.remove("dark");
+        array2.remove("btn-close-white")
     } else {
         array.add("dark");
+        array2.add("btn-close-white")
     }
 
 }
